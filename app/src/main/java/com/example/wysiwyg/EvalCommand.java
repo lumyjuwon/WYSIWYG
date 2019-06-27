4efe8fce445f04bd217fc753a97ef960636b41be
lumyjuwon <lumyjuwon@gmail.com>
2019-06-27 18:28:52 +0900

    Remove '

--- app/src/main/java/com/example/wysiwyg/EvalCommand.java
+++ app/src/main/java/com/example/wysiwyg/EvalCommand.java
@@ -1,21 +1,21 @@
 package com.example.wysiwyg;
 
 public enum EvalCommand{
-    FONTNAME("'fontName'"),
-    FONTSIZE("'fontSize'"),
-    BOLD("'bold'"),
-    UNDERLINE("'underline'"),
-    ITALIC("'italic'"),
-    STRIKETHROUGH("'strikeThrough'"),
-    SUPERSCRIPT("'superscript'"),
-    SUBSCRIPT("'subscript'"),
-    FORECOLOR("'foreColor'"),
-    BACKCOLOR("'backColor'"),
-    JUSTIFYLEFT("'justifyLeft'"),
-    JUSTIFYCENTER("'justifyCenter'"),
-    JUSTIFYRIGHT("'justifyRight'"),
-    INSERTUNORDEREDLIST("'insertUnorderedList'"),
-    INSERTORDEREDLIST("'insertOrderedList'");
+    FONTNAME("fontName"),
+    FONTSIZE("fontSize"),
+    BOLD("bold"),
+    UNDERLINE("underline"),
+    ITALIC("italic"),
+    STRIKETHROUGH("strikeThrough"),
+    SUPERSCRIPT("superscript"),
+    SUBSCRIPT("subscript"),
+    FORECOLOR("foreColor"),
+    BACKCOLOR("backColor"),
+    JUSTIFYLEFT("justifyLeft"),
+    JUSTIFYCENTER("justifyCenter"),
+    JUSTIFYRIGHT("justifyRight"),
+    INSERTUNORDEREDLIST("insertUnorderedList"),
+    INSERTORDEREDLIST("insertOrderedList");
     
     private final String cmd;
 
