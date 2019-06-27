81cb9c157bd6c155dda0b5c15fdf8842d704eef6
lumyjuwon <lumyjuwon@gmail.com>
2019-06-27 16:33:00 +0900

    Get CallBack

--- app/src/main/java/com/example/wysiwyg/WysiwygWebView.java
+++ app/src/main/java/com/example/wysiwyg/WysiwygWebView.java
@@ -1,21 +1,27 @@
 package com.example.wysiwyg;
 
+import android.util.Log;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import org.json.JSONObject;
 
 public class WysiwygWebView extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+        JsonDecoder jsonDecoder = JsonDecoder.decode(request.getUrl().toString());
 
-        System.out.println("request " + request);
+        try {
+            // Convert Object type to String
+            System.out.println(jsonDecoder.get(EvalCommand.BOLD.toString()));
+        }
+        catch(Exception e){
+            Log.d("JsonDecoder", e.toString());
+        }
 
         return true;
     }
-
-
-
-
 
 }
