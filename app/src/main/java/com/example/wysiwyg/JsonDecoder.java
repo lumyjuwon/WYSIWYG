0c9da99a9fd3b7dfbebe7f5887e22e890b4d9a43
kaistyoon <hsy6119@naver.com>
2019-06-28 06:39:31 +0900

    stateMap receive Object type value

--- app/src/main/java/com/example/wysiwyg/JsonDecoder.java
+++ app/src/main/java/com/example/wysiwyg/JsonDecoder.java
@@ -26,12 +26,41 @@ public class JsonDecoder {
         return jsonDecoder;
     }
 
-    public String getValue(String key){
+    public Object getValue(String key){
         try {
             // Convert Object type to String
             items = new JSONObject(mJson.toUpperCase());
             //if(key.equals("BOLD")) System.out.println(mJson);
-            return items.get(key).toString();
+            String valStr = items.get(key).toString().toLowerCase();
+            if(key.equals("FONTNAME")){
+                if(valStr.equals("null")) return "arail"; //기본 폰트로 변경 필요
+                return valStr;
+            }
+            else if(key.equals("FONTSIZE")||key.equals("LINEHEIGHT")){
+                if(valStr.equals("null")) return "14"; //기본 폰트 크기로 변경 필요
+                return Float.parseFloat(valStr);
+            }
+            else if(key.equals("FORECOLOR")) {
+                if(valStr.equals("null")) {
+                    int[] val = {0,0,0}; //기본 배경색으로 변경 필요
+                    return val;
+                }
+                valStr = valStr.substring(4,valStr.length()-1);
+                String[] valStrArr = valStr.split("(,\\s)");
+                int[] valIntArr = {Integer.parseInt(valStrArr[0]), Integer.parseInt(valStrArr[1]), Integer.parseInt(valStrArr[2])};
+                return valIntArr;
+            }
+            else if(key.equals("BACKCOLOR")) {
+                if(valStr.equals("null")) {
+                    int[] val = {0,0,0,255}; //투명 배경색으로 변경 필요
+                    return val;
+                }                  valStr = valStr.substring(5,valStr.length()-1);
+                String[] valStrArr = valStr.split("(,\\s)");
+                int[] valIntArr = {Integer.parseInt(valStrArr[0]), Integer.parseInt(valStrArr[1]), Integer.parseInt(valStrArr[2]), Integer.parseInt(valStrArr[3])};
+                return valIntArr;
+            }
+            if(valStr.equals("true")) return true;
+            return false;
         }
         catch(Exception e){
             Log.d("JsonDecoder", e.toString());
--- app/src/main/java/com/example/wysiwyg/WysiwygWebView.java
+++ app/src/main/java/com/example/wysiwyg/WysiwygWebView.java
@@ -8,7 +8,7 @@ import android.webkit.WebViewClient;
 import java.util.HashMap;
 
 public class WysiwygWebView extends WebViewClient{
-    private HashMap<String, String> stateMap = new HashMap<String, String>();
+    private HashMap<String, Object> stateMap = new HashMap<String, Object>();
 
     public interface StateEventListener{
         void onReceivedEvent(String str);
@@ -30,14 +30,22 @@ public class WysiwygWebView extends WebViewClient{
             try {
                 // Convert Object type to String
                 // stateEventListener.onReceivedEvent(jsonDecoder.getValue(EvalCommand.BOLD.toString()));
-                /*if(cmd.name().equals("BOLD")) {
+                /*if(cmd.name().equals("FORECOLOR")) {
                     System.out.print(cmd.name());
                     System.out.print("/");
                     Object tmpobj = jsonDecoder.getValue(cmd.name());
-                    System.out.print(tmpobj);
+                    //String valStr = ((String)tmpobj).substring(4,((String)tmpobj).length()-1);
+                    String valStr = ("RGB(255, 33, 222)").substring(4,("RGB(255, 33, 222)").length()-1);
+                    String[] valStrArr = valStr.split("(,\\s)");
+                    int[] valIntArr = {Integer.parseInt(valStrArr[0]),Integer.parseInt(valStrArr[1]),Integer.parseInt(valStrArr[2])};
+                    System.out.print(valIntArr[0]);//tmpobj);
+                    System.out.print(",");
+                    System.out.print(valIntArr[1]);
+                    System.out.print(",");
+                    System.out.print(valIntArr[2]);
                     System.out.print("/");
                     System.out.println(tmpobj instanceof String);
-                }*/
+                }//*/
             }
             catch (Exception e) {
                 Log.d("JsonDecoder", e.toString());
