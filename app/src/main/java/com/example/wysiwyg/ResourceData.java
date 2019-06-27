87d4cde07d9589f54d38abb99afb83535e1269fb
lumyjuwon <lumyjuwon@gmail.com>
2019-06-28 07:11:58 +0900

    Separate EvalMethod
    Resource Data are saved in ResourceData

---
+++ app/src/main/java/com/example/wysiwyg/ResourceData.java
@@ -0,0 +1,26 @@
+package com.example.wysiwyg;
+
+public enum ResourceData {
+
+    BLACK("black"),
+    WHITE("white"),
+    RED("red"),
+    BLUE("blue"),
+    GREEN("green"),
+    NINEFONTSIZE("1"),
+    TENFONTSIZE("2"),
+    ELEVENFONTSIZE("3"),
+    TWENTEENFONTSIZE("5"),
+    FOURTEENFONTSIZE("7");
+
+    private final String resource;
+
+    private ResourceData(String resource){
+        this.resource = resource;
+    }
+    @Override
+    public String toString(){
+        return this.resource;
+    }
+
+}
