3baa47f8fb671a27ea0dc7ad32aa20dd9bb7276a
lumyjuwon <lumyjuwon@gmail.com>
2019-06-27 18:29:38 +0900

    Add Caller

--- app/src/main/java/com/example/wysiwyg/WysiwygEditor.java
+++ app/src/main/java/com/example/wysiwyg/WysiwygEditor.java
@@ -10,8 +10,9 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
-public class WysiwygEditor extends LinearLayout{
+public class WysiwygEditor extends LinearLayout implements WysiwygWebView.StateEventListener{
     private final String SET_HTML = "file:///android_asset/editor.html";
+    private WysiwygWebView wysiwygWebView;
 
     @SuppressWarnings("FieldCanBeLocal")
     private WebView mWebView;
@@ -61,44 +62,43 @@ public class WysiwygEditor extends LinearLayout{
         // WebView
         mWebView = findViewById(R.id.webview);
         mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.setWebViewClient(new WysiwygWebView());
+        // State Listener
+        wysiwygWebView = new WysiwygWebView();
+        wysiwygWebView.setStateEventListener(this);
+
+        mWebView.setWebViewClient(wysiwygWebView);
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
 
         mWebView.loadUrl(SET_HTML);
 
         // Editor Button
+        EditorButton.setGlobalCheckedColorFilter(R.color.colorBackBlue);
+        EditorButton.setGlobalUnCheckedColorFilter(R.color.colorTextBlack);
         EditorButton.setGlobalOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Type down casting
+                EditorButton btn = (EditorButton) v;
+
+                // Change Color Filter
+                btn.changeClicked();
+                btn.changeColorFilter();
+
+                // Function
                 switch(v.getId()){
                     case R.id.btn_size:
                         break;
                     case R.id.btn_color:
                         break;
-                    case R.id.btn_bgcolor:
-                        break;
                     case R.id.btn_bold:
-                        mWebView.evaluateJavascript("javascript:WE.exec(" + EvalCommand.BOLD.toString() + ");", null);
+                        mWebView.evaluateJavascript("javascript:WE.exec('" + EvalCommand.BOLD.toString() + "');", null);
                         break;
                     case R.id.btn_italic:
-                        mWebView.evaluateJavascript("javascript:WE.exec(" + EvalCommand.ITALIC.toString() + ");", null);
+                        mWebView.evaluateJavascript("javascript:WE.exec('" + EvalCommand.ITALIC.toString() + "');", null);
                         break;
                     case R.id.btn_under:
-                        mWebView.evaluateJavascript("javascript:WE.exec(" + EvalCommand.UNDERLINE.toString() + ");", null);
-                        break;
-                    case R.id.btn_strike:
-                        mWebView.evaluateJavascript("javascript:WE.exec(" + EvalCommand.STRIKETHROUGH.toString() + ");", null);
-                        break;
-                    case R.id.btn_align:
-                        break;
-                    case R.id.btn_img:
-                        break;
-                    case R.id.btn_video:
-                        break;
-                    case R.id.btn_cfrm:
-                        break;
-                    case R.id.btn_cancel:
+                        mWebView.evaluateJavascript("javascript:WE.exec('" + EvalCommand.UNDERLINE.toString() + "');", null);
                         break;
                 }
             }
@@ -106,12 +106,13 @@ public class WysiwygEditor extends LinearLayout{
 
         mButtonSize = findViewById(R.id.btn_size);
         mButtonSize.setOnClickListener(EditorButton.getOnClickListener());
+        // mButtonSize.setColorFilter
 
         mButtonColor = findViewById(R.id.btn_color);
         mButtonColor.setOnClickListener(EditorButton.getOnClickListener());
 
-        mButtonBgColor = findViewById(R.id.btn_bgcolor);
-        mButtonBgColor.setOnClickListener(EditorButton.getOnClickListener());
+//        mButtonBgColor = findViewById(R.id.btn_bgcolor);
+//        mButtonBgColor.setOnClickListener(EditorButton.getOnClickListener());
 
         mButtonBold = findViewById(R.id.btn_bold);
         mButtonBold.setOnClickListener(EditorButton.getOnClickListener());
@@ -122,24 +123,29 @@ public class WysiwygEditor extends LinearLayout{
         mButtonUnder = findViewById(R.id.btn_under);
         mButtonUnder.setOnClickListener(EditorButton.getOnClickListener());
 
-        mButtonStrike = findViewById(R.id.btn_strike);
-        mButtonStrike.setOnClickListener(EditorButton.getOnClickListener());
+//        mButtonStrike = findViewById(R.id.btn_strike);
+//        mButtonStrike.setOnClickListener(EditorButton.getOnClickListener());
 
         mButtonAlign = findViewById(R.id.btn_align);
         mButtonAlign.setOnClickListener(EditorButton.getOnClickListener());
 
-        mButtonImg = findViewById(R.id.btn_img);
-        mButtonImg.setOnClickListener(EditorButton.getOnClickListener());
-
-        mButtonVideo = findViewById(R.id.btn_video);
-        mButtonVideo.setOnClickListener(EditorButton.getOnClickListener());
+//        mButtonImg = findViewById(R.id.btn_img);
+//        mButtonImg.setOnClickListener(EditorButton.getOnClickListener());
+//
+//        mButtonVideo = findViewById(R.id.btn_video);
+//        mButtonVideo.setOnClickListener(EditorButton.getOnClickListener());
 
         mButtonConfirm = findViewById(R.id.btn_cfrm);
         mButtonConfirm.setOnClickListener(EditorButton.getOnClickListener());
 
-        mButtonCancel = findViewById(R.id.btn_cancel);
-        mButtonCancel.setOnClickListener(EditorButton.getOnClickListener());
+//        mButtonCancel = findViewById(R.id.btn_cancel);
+//        mButtonCancel.setOnClickListener(EditorButton.getOnClickListener());
 
+    }
+
+    @Override
+    public void onReceivedEvent(String state) {
+        System.out.println("Listener " + state);
     }
 
 }
