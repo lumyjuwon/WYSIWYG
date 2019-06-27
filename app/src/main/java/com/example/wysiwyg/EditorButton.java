c6de29f429249167d3f9aaf9b5e7a1118788bdff
lumyjuwon <lumyjuwon@gmail.com>
2019-06-27 18:28:38 +0900

    Add Listener

--- app/src/main/java/com/example/wysiwyg/EditorButton.java
+++ app/src/main/java/com/example/wysiwyg/EditorButton.java
@@ -1,15 +1,22 @@
 package com.example.wysiwyg;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatImageButton;
 
 public class EditorButton extends AppCompatImageButton {
 
+    public interface ClickedListener{
+        void onReceiveClickedListener();
+    }
+
+    private ClickedListener mClickedListener;
     private static OnClickListener mOnClickListener;
     private static int CheckedColorFilter;
     private static int UnCheckedColorFilter;
+    private boolean isChecked = false;
 
     public EditorButton(Context context){
         super(context);
@@ -33,6 +40,33 @@ public class EditorButton extends AppCompatImageButton {
 
     public static OnClickListener getOnClickListener(){
         return mOnClickListener;
+    }
+
+    public static int getCheckedColorFilter(){
+        return CheckedColorFilter;
+    }
+
+    public static int getUnCheckedColorFilter(){
+        return UnCheckedColorFilter;
+    }
+
+    public void changeClicked(){
+        if(isChecked)
+            isChecked = false;
+        else
+            isChecked = true;
+        System.out.println(isChecked);
+    }
+
+    public void changeColorFilter(){
+        if(isChecked)
+            this.setColorFilter(R.color.colorPrimary);
+        else
+            this.setColorFilter(R.color.colorBackYellow);
+    }
+
+    public void setOnClickedListener(EditorButton.ClickedListener listener){
+        this.mClickedListener = listener;
     }
 
 }
